64128367bce86e65c94e8470224bf4c9fcab53b9
ug595 <ug595@yahoo.com.au>
2024-01-25 03:22:58 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GatsbySite`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"]
+};
